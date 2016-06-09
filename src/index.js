6864980782809bec2ee5624ce123d95ae7968dcd
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-06-09 16:33:34 -0700

    completed updating a video and created a call back click handler for the occasion

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 var CONTAINER = document.querySelector('.container');
 const API_KEY = 'AIzaSyACruflsQ1asyTDX_BdX5S8thDIu5rgFhs';
 
 class App extends Component {
 	constructor(props){
 		super(props);
-		this.state = {videos: [] };
+		this.state = {
+			videos: [],
+			selectedVideo: null
+			 };
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({ 
+				videos:videos,
+				selectedVideo: videos[0]
+		 });
 		});
 	}
 
@@ -20,7 +27,10 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
